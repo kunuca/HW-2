5dde20fcc9a892bbad7c3141a86db3c65222a180
Tatiana Kustova <kunuca@mail.ru>
2023-02-06 12:26:17 +0400

    001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите число дня недели");
+int day=int.Parse(Console.ReadLine());
+day=day-1;
+string[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+if (day<5) Console.WriteLine($"{week[day]} - не выходной день");
+else Console.WriteLine($"{week[day]} - выходной день");
